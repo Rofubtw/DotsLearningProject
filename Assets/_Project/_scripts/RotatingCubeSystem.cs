78954eb8ae0481b12ef2b04b63c829fd9a4e91dd
unknown <raufyl37@gmail.com>
2024-03-01 20:28:55 +0300

    ECS Burst and Job System

---
+++ Assets/_Project/_scripts/RotatingCubeSystem.cs
@@ -0,0 +1,41 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace LearningDots
+{
+    public partial struct RotatingCubeSystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<RotateSpeed>();
+        }
+        
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            /*
+            foreach (var (localTransform, rotateSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>>())
+            {
+                localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime);
+            }
+            */
+            var rotatingCubeJob = new RotatingCubeJob
+            {
+                deltaTime = SystemAPI.Time.DeltaTime
+            };
+            rotatingCubeJob.Schedule();
+        }
+    }
+
+    [BurstCompile]
+    public partial struct RotatingCubeJob : IJobEntity
+    {
+        public float deltaTime;
+        public void Execute(ref LocalTransform localTransform, in RotateSpeed rotateSpeed)
+        {
+            localTransform = localTransform.RotateY(rotateSpeed.value * deltaTime);
+        }
+    }
+}
